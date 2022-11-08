f9daf55cf515d7253af5b39c2118b7817e8e71ab
Nhin <nhinlechi@gmail.com>
2022-11-08 21:22:27 +0700

    Implement send message api

--- chat_app/rpc_api/chat.go
+++ chat_app/rpc_api/chat.go
@@ -1,6 +1,8 @@
 package rpcapi
 
 import (
+	"context"
+	"sync"
 	"v1/pb"
 )
 
@@ -14,4 +16,36 @@ func (server *Server) Subscribe(req *pb.SubscribeRequest, srv pb.ChatService_Sub
 	server.connections[req.UserId] = conn
 
 	return <-conn.errors
+}
+
+func (server *Server) SendMessage(ctx context.Context, req *pb.SendMessageRequest) (*pb.SendMessageResponse, error) {
+	// Filter all connections with user_id in conversation
+
+	event := &pb.NewMessageEvent{
+		Content:        req.Content,
+		MessageId:      "MessageID",
+		ConversationId: req.ConversationId,
+	}
+
+	wait := sync.WaitGroup{}
+	done := make(chan int)
+
+	for _, conn := range server.connections {
+		wait.Add(1)
+
+		go func(event *pb.Event, conn *Connection) {
+			// TODO: implement send message logic here
+		}(&pb.Event{Event: &pb.Event_Message{Message: event}}, conn)
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+	<-done
+
+	resp := &pb.SendMessageResponse{
+		MessageId: "MessageID",
+	}
+	return resp, nil
 }

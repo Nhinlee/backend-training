5e6a6b138116bed5181a654e56149baba8558e26
Nhin <nhinlechi@gmail.com>
2022-11-12 16:27:40 +0700

    Implement test for user

---
+++ chat_app/db/sqlc/sqlc_test/main_test.go
@@ -0,0 +1,30 @@
+package sqlc_test
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+	"v1/config"
+	db "v1/db/sqlc"
+)
+
+var testQueries *db.Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	// load configs
+	config, err := config.LoadConfig("../../..")
+	if err != nil {
+		return
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	testQueries = db.New(testDB)
+
+	os.Exit(m.Run())
+}
---
+++ chat_app/db/sqlc/sqlc_test/user_test.go
@@ -0,0 +1,55 @@
+package sqlc_test
+
+import (
+	"context"
+	"testing"
+	core "v1/core"
+	db "v1/db/sqlc"
+	utils "v1/utils"
+
+	"github.com/stretchr/testify/require"
+)
+
+func GetRandomUser() db.User {
+	firstName := "test " + utils.RandomString(10)
+	lastName := "test " + utils.RandomString(10)
+	email := "test+" + utils.RandomString(10) + "@gmail.com"
+	password := utils.RandomString(10)
+	hashedPassword, _ := core.HashPassword(password)
+
+	return db.User{
+		FirstName:      firstName,
+		LastName:       lastName,
+		Email:          email,
+		HashedPassword: hashedPassword,
+	}
+}
+
+func CreateRandomUser(t *testing.T) db.User {
+	randomUser := GetRandomUser()
+
+	arg := db.CreateUserParams{
+		FirstName:      randomUser.FirstName,
+		LastName:       randomUser.LastName,
+		Email:          randomUser.Email,
+		HashedPassword: randomUser.HashedPassword,
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.FirstName, user.FirstName)
+	require.Equal(t, arg.LastName, user.LastName)
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+
+	require.NotZero(t, user.UserID)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	CreateRandomUser(t)
+}

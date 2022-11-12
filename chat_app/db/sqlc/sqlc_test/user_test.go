b635348a329b045dd1c69b9f6c47e642ed92340c
Nhin <nhinlechi@gmail.com>
2022-11-12 16:47:56 +0700

    Update logic for conversation_members query

--- chat_app/db/sqlc/sqlc_test/user_test.go
+++ chat_app/db/sqlc/sqlc_test/user_test.go
@@ -56,3 +56,33 @@ func CreateRandomUser(t *testing.T) db.User {
 func TestCreateUser(t *testing.T) {
 	CreateRandomUser(t)
 }
+
+func TestGetUser(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Email)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1, user2)
+}
+
+func TestUpdateUser(t *testing.T) {
+	user1 := CreateRandomUser(t)
+
+	arg := db.UpdateUserInfoParams{
+		UserID:    user1.UserID,
+		FirstName: user1.FirstName + "updated",
+		LastName:  user1.LastName + "updated",
+	}
+
+	user2, err := testQueries.UpdateUserInfo(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user2.Email, user1.Email)
+	require.Equal(t, user2.UserID, user1.UserID)
+	require.Equal(t, user2.FirstName, arg.FirstName)
+	require.Equal(t, user2.LastName, arg.LastName)
+}
